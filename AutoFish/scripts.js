8e8ed58aae5476fb370d350acf21da68a93ed60f
metroidator <164851694+metroidator@users.noreply.github.com>
2024-03-25 16:53:47 +0100

    changed script.js

--- AutoFish/scripts.js
+++ AutoFish/scripts.js
@@ -3,4 +3,18 @@ const navShopButton = document.getElementById("navShopButton");
 const navNosProjetsButton = document.getElementById("navNosProjetsButton");
 const navContactButton = document.getElementById("navContactButton");
 
-for()
+navHomeButton.addEventListener("click", (event) => {
+    document.location.href="index.html";
+});
+
+navStoreButton.addEventListener("click", (event) => {
+    document.location.href="store.html";
+});
+
+navProjectsButton.addEventListener("click", (event) => {
+    document.location.href="projects.html";
+});
+
+navContactButton.addEventListener("click", (event) => {
+    document.location.href="contact.html";
+});
