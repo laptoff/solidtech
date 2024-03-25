0f7a5802ef3396f785df99653785148ad162d3e3
metroidator <164851694+metroidator@users.noreply.github.com>
2024-03-25 16:51:47 +0100

    init script.js

---
+++ AutoFish/scripts.js
@@ -0,0 +1,6 @@
+const navAccueilButton = document.getElementById("navAccueilButton");
+const navShopButton = document.getElementById("navShopButton");
+const navNosProjetsButton = document.getElementById("navNosProjetsButton");
+const navContactButton = document.getElementById("navContactButton");
+
+for()
