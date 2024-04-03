4e30f4507822499725d2470c1c9d985b12a8b831
azerty841 <164852097+azerty841@users.noreply.github.com>
2024-04-03 12:00:25 +0200

    add connection popup to camcar page.

---
+++ CamCar/script,car.js
@@ -0,0 +1,25 @@
+const navHomeButton = document.getElementById("navHomeButton");
+const navStoreButton = document.getElementById("navStoreButton");
+const navProjectsButton = document.getElementById("navProjectsButton");
+const navContactButton = document.getElementById("navContactButton");
+const navLogo = document.getElementById("logo");
+
+navHomeButton.addEventListener("click", (event) => {
+    document.location.href="/index.html";
+});
+
+navStoreButton.addEventListener("click", (event) => {
+    document.location.href="/otherPages/store.html";
+});
+
+navProjectsButton.addEventListener("click", (event) => {
+    document.location.href="/otherPages/projects.html";
+});
+
+navContactButton.addEventListener("click", (event) => {
+    document.location.href="/otherPages/contact.html";
+});
+
+navLogo.addEventListener("click", (event) => {
+    document.location.href="/index.html";
+});
