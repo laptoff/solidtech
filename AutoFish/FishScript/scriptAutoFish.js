636eba8f86507b8c84d746a3a4f1fa85f6e79ef0
metroidator <164851694+metroidator@users.noreply.github.com>
2024-04-15 09:44:03 +0200

    Autofish update lol

--- AutoFish/FishScript/scriptAutoFish.js
+++ AutoFish/FishScript/scriptAutoFish.js
@@ -2,19 +2,24 @@ const navAccueilButton = document.getElementById("navAccueilButton");
 const navShopButton = document.getElementById("navShopButton");
 const navNosProjetsButton = document.getElementById("navNosProjetsButton");
 const navContactButton = document.getElementById("navContactButton");
+const navLogo = document.getElementById("logo");
 
 navAccueilButton.addEventListener("click", (event) => {
-    document.location.href="../AutoFish/Autofish.html";
+    document.location.href="/AutoFish/Autofish.html";
 });
 
 navShopButton.addEventListener("click", (event) => {
-    document.location.href="../AutoFish/OtherFishPages/Shop.html";
+    document.location.href="/AutoFish/OtherFishPages/Shop.html";
 });
 
 navNosProjetsButton.addEventListener("click", (event) => {
-    document.location.href="../AutoFish/OtherFishPages/NosProjets.html";
+    document.location.href="/AutoFish/OtherFishPages/NosProjets.html";
 });
 
 navContactButton.addEventListener("click", (event) => {
-    document.location.href="../AutoFish/OtherFishPages/Contact.html";
+    document.location.href="/AutoFish/OtherFishPages/Contact.html";
+});
+
+navLogo.addEventListener("click", (event) => {
+    document.location.href="../index.html";
 });
