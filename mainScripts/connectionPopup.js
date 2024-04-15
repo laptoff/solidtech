5c7bff42dec85e860e2d74420d32f9bc6e1ac8be
laptoff <laptoff.contact@gmail.com>
2024-04-15 09:46:14 +0200

    delete connection popup content for a reworking

--- mainScripts/connectionPopup.js
+++ mainScripts/connectionPopup.js
@@ -1,4 +1,3 @@
-const cross = document.getElementById("cross_icon");
 const connectionPopup = document.getElementById("connection_popup");
 const connectionButton = document.getElementById("connection_icon");
 const connectionTitle = document.getElementById("connection_title_wrap");
@@ -7,19 +6,19 @@ const inputsPassword = document.getElementById("connection_password_inputs");
 const registerWrap = document.getElementById("register_wrap");
 const confirmButton = document.getElementById("confirm_button_warp");
 
-cross.addEventListener("click", (event) => {
-    connectionPopup.classList.remove("connection_popup_active");
-    cross.classList.remove("connection_cross_active");
-    connectionTitle.classList.remove("connection_title_wrap_active");
-    inputsMail.classList.remove("inputs_active");
-    inputsPassword.classList.remove("inputs_active");
-    registerWrap.classList.remove("register_wrap_active");
-    confirmButton.classList.remove("confirm_button_warp_active");
+window.addEventListener("keydown", function(e) {
+    if (e.key == "Escape") {
+        connectionPopup.classList.remove("connection_popup_active");
+        connectionTitle.classList.remove("connection_title_wrap_active");
+        inputsMail.classList.remove("inputs_active");
+        inputsPassword.classList.remove("inputs_active");
+        registerWrap.classList.remove("register_wrap_active");
+        confirmButton.classList.remove("confirm_button_warp_active");
+    }
 });
 
 connectionButton.addEventListener("click", (event) => {
     connectionPopup.classList.add("connection_popup_active");
-    cross.classList.add("connection_cross_active");
     connectionTitle.classList.add("connection_title_wrap_active");
     inputsMail.classList.add("inputs_active");
     inputsPassword.classList.add("inputs_active");
