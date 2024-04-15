a105fc5fdd5b90b446b83b49f971a28d8dc6ad9a
metroidator <164851694+metroidator@users.noreply.github.com>
2024-04-15 09:05:53 +0200

    FishScript created

--- AutoFish/FishScript/scriptAutoFish.js
+++ AutoFish/FishScript/scriptAutoFish.js
@@ -4,17 +4,17 @@ const navNosProjetsButton = document.getElementById("navNosProjetsButton");
 const navContactButton = document.getElementById("navContactButton");
 
 navAccueilButton.addEventListener("click", (event) => {
-    document.location.href="/AutoFish/Autofish.html";
+    document.location.href="../AutoFish/Autofish.html";
 });
 
 navShopButton.addEventListener("click", (event) => {
-    document.location.href="/AutoFish/OtherFishPages/Shop.html";
+    document.location.href="../AutoFish/OtherFishPages/Shop.html";
 });
 
 navNosProjetsButton.addEventListener("click", (event) => {
-    document.location.href="/AutoFish/OtherFishPages/NosProjets.html";
+    document.location.href="../AutoFish/OtherFishPages/NosProjets.html";
 });
 
 navContactButton.addEventListener("click", (event) => {
-    document.location.href="/AutoFish/OtherFishPages/Contact.html";
+    document.location.href="../AutoFish/OtherFishPages/Contact.html";
 });
